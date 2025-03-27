b4513bd3c22ac239b5db8a348f5d2e5b0d4fdc4c
Tom Palmer <remlapmot@hotmail.com>
2025-03-27 06:22:10 +0000

    Update build.R

--- build.R
+++ build.R
@@ -1,28 +1,42 @@
 options(
   repos = c(
     universe = "https://mrcieu.r-universe.dev/",
-    binaries = "https://p3m.dev/cran/__linux__/noble/2025-03-24"
+    CRAN = "https://p3m.dev/cran/__linux__/noble/2025-03-24"
   )
 )
 
 if (R.version["arch"] != "aarch64") {
   options(
-      HTTPUserAgent = sprintf(
-          'R/%s R (%s)',
-          getRversion(),
-          paste(
-              getRversion(),
-              R.version['platform'],
-              R.version['arch'],
-              R.version['os']
-          )
+    HTTPUserAgent = sprintf(
+      'R/%s R (%s)',
+      getRversion(),
+      paste(
+        getRversion(),
+        R.version['platform'],
+        R.version['arch'],
+        R.version['os']
       )
+    )
   )
 }
 
-install.packages("pak")
+# install prebuilt binary pak from their repo
+install.packages(
+  "pak",
+  repos = sprintf(
+    "https://r-lib.github.io/p/pak/stable/%s/%s/%s",
+    .Platform$pkgType,
+    R.Version()$os,
+    R.Version()$arch
+  )
+)
 
+# install TwoSampleMR and hard and soft deps
+pak::repo_get()
+stop("stop")
 pak::pkg_install("TwoSampleMR", dependencies = TRUE)
+
+
 
 # mr.raps suggests packages from BioConductor
 pak::pkg_install(c("bumphunter", "TxDb.Hsapiens.UCSC.hg38.knownGene"))
