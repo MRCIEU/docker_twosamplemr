12cc78324ffd551cd5157c6138c17797e4cadc5f
Tom Palmer <remlapmot@hotmail.com>
2025-05-05 14:39:26 +0100

    Update build with r-universe arch URLs

--- build.R
+++ build.R
@@ -1,38 +1,9 @@
-# source package repos for aarch64
+# Amend pkgdepends sysreqs database timeout
 options(
-  repos = c(
-    universe = "https://mrcieu.r-universe.dev/",
-    CRAN = "https://p3m.dev/cran/2025-04-30"
-  ),
   pkg.sysreqs_db_update_timeout = as.difftime(59, units = "secs")
 )
 
-# Set the Bioconductor version to prevent defaulting to a newer version:
-Sys.setenv("R_BIOC_VERSION" = "3.21")
-
-if (R.Version()$arch == "x86_64") {
-  # Linux binary package repos for x86_64
-  options(
-    repos = c(
-      universe = "https://mrcieu.r-universe.dev/bin/linux/noble/4.5/",
-      CRAN = "https://p3m.dev/cran/__linux__/noble/2025-04-30",
-      BioCsoft = "https://packagemanager.posit.co/bioconductor/__linux__/noble/2025-04-29"
-    ),
-    BIOCONDUCTOR_CONFIG_FILE = "https://packagemanager.posit.co/bioconductor/2025-04-29/config.yaml",
-    HTTPUserAgent = sprintf(
-      'R/%s R (%s)',
-      getRversion(),
-      paste(
-        getRversion(),
-        R.version['platform'],
-        R.version['arch'],
-        R.version['os']
-      )
-    )
-  )
-}
-
-# install prebuilt binary pak from their repo
+# install prebuilt binary pak from pak repo
 install.packages(
   "pak",
   repos = sprintf(
@@ -43,10 +14,47 @@ install.packages(
   )
 )
 
+# Set the Bioconductor version to prevent defaulting to a newer version:
+Sys.setenv("R_BIOC_VERSION" = "3.21")
+
+# Setup package URLs for x86_64
+if (R.Version()$arch == "x86_64") {
+  # Linux binary package repos for x86_64
+  pak::repo_add(universe = "https://mrcieu.r-universe.dev/bin/linux/noble-x86_64/4.5/")
+  pak::repo_add(CRAN = "https://p3m.dev/cran/__linux__/noble/2025-04-30")
+  pak::repo_add(BioCsoft = "https://packagemanager.posit.co/bioconductor/__linux__/noble/2025-04-29")
+}
+
+# Setup package URLs for ARM/AARCH64
+if (R.Version()$arch == "aarch64") {
+  pak::repo_add(universe = "https://mrcieu.r-universe.dev/bin/linux/noble-aarch64/4.5/")
+  pak::repo_add(CRAN = "https://p3m.dev/cran/2025-04-30")
+  pak::repo_add(BioCsoft = "https://packagemanager.posit.co/bioconductor/2025-04-29")
+}
+
+# Set HTTPUserAgent to obtain binary packages from Posit Public Package Manager on x86_64
+options(
+  HTTPUserAgent = sprintf(
+    'R/%s R (%s)',
+    getRversion(),
+    paste(
+      getRversion(),
+      R.version['platform'],
+      R.version['arch'],
+      R.version['os']
+    )
+  )
+)
+
+options(
+  BIOCONDUCTOR_CONFIG_FILE = "https://packagemanager.posit.co/bioconductor/2025-04-29/config.yaml"
+)
+
 # install TwoSampleMR and hard and soft deps
 pak::pkg_install("TwoSampleMR", dependencies = TRUE)
 
-# mr.raps suggests packages from BioConductor
+# install mr.raps Suggests packages from BioConductor
 pak::pkg_install(c("bumphunter", "TxDb.Hsapiens.UCSC.hg38.knownGene"))
 
+# Uninstall pak
 remove.packages("pak")
