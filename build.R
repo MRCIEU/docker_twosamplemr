678b22797eaee36ced7f56497039f75d81e11a83
Tom Palmer <remlapmot@hotmail.com>
2026-06-24 10:39:14 +0100

    Bump date

--- build.R
+++ build.R
@@ -3,7 +3,7 @@ options(
   pkg.sysreqs_db_update_timeout = as.difftime(59, units = "secs")
 )
 
-cran_bioc_date <- "2026-06-10"
+cran_bioc_date <- "2026-06-24"
 
 # install prebuilt binary pak from pak repo
 install.packages(
